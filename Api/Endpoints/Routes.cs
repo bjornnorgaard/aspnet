59def4eb87faf70310a11cddda6590d24b26c894
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2025-08-10 12:48:57 +0200

    Add `GetTodoListEndpoint` for retrieving all todo items
    - Introduced new endpoint `GetTodoListEndpoint` implementing `AbstractEndpoint` to handle GET requests for all todos.
    - Extended `Routes` with `GetTodoList` constant for route management.

--- Api/Endpoints/Routes.cs
+++ Api/Endpoints/Routes.cs
@@ -3,4 +3,5 @@ namespace Api.Endpoints;
 public static class Routes
 {
     public const string GetTodo = "get-todo";
+    public const string GetTodoList = "get-todo-list";
 }
---
+++ Api/Endpoints/Todos/GetTodoListEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Platform.Reflection;
+
+namespace Api.Endpoints.Todos;
+
+public class GetTodoListEndpoint(ILogger<GetTodoListEndpoint> logger) : AbstractEndpoint
+{
+    public override EndpointDefinition Definition()
+    {
+        return new EndpointDefinition
+        {
+            Group = Groups.Todos,
+            Method = HttpMethod.Get,
+            Version = Versions.V1,
+            Route = Routes.GetTodoList,
+            Description = "Retrieves all todo items."
+        };
+    }
+
+    public override IResult Handle()
+    {
+        logger.LogInformation("Handling GetTodos request.");
+        return Results.Ok("All todo items retrieved successfully.");
+    }
+}
