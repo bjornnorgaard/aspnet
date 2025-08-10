cd8825daf403030f835565cc814bf49c19c43219
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2025-08-10 17:52:55 +0200

    Refactor `GetTodoListEndpoint` to change HTTP method to POST and update request handling
    - Changed HTTP method from GET to POST.
    - Added `Request` record to handle input parameters.
    - Updated `Handle` delegate to use `[FromBody]` and align with the new request model.

--- Api/Endpoints/Todos/GetTodoListEndpoint.cs
+++ Api/Endpoints/Todos/GetTodoListEndpoint.cs
@@ -1,24 +1,28 @@
 ﻿using Api.Endpoints.Constants;
 using Api.Features.Todos;
+using Microsoft.AspNetCore.Mvc;
 using Platform.Reflection;
 
 namespace Api.Endpoints.Todos;
 
-public class GetTodoListEndpoint(GetTodoList.Handler feature) : AbstractEndpoint
+public class GetTodoListEndpoint : AbstractEndpoint
 {
     protected override EndpointDefinition Definition() => new()
     {
         Group = Groups.Todos,
-        Method = HttpMethod.Get,
+        Method = HttpMethod.Post,
         Version = Versions.V1,
         Route = "get-todo-list",
         Description = "Retrieves all todo items."
     };
 
-    protected override Delegate Handle => async (int limit, CancellationToken ct) =>
+    public record Request(int Limit);
+
+    protected override Delegate Handle => async ([FromServices] GetTodoList.Handler feature, [FromBody] Request request,
+        CancellationToken ct) =>
     {
-        var request = new GetTodoList.Request { Limit = limit };
-        var result = await feature.Handle(request, ct);
+        var req = new GetTodoList.Request { Limit = request.Limit };
+        var result = await feature.Handle(req, ct);
         return Results.Ok(result.List);
     };
 }
