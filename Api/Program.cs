08399f834ac21cd563e0dc49e63772dc7adcd1b0
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2025-08-12 21:17:40 +0200

    Remove unused imports from `Platform` and `Program` classes
    - Cleaned up redundant `using` directives in `Platform.cs` and `Program.cs`.
    - Simplified codebase by removing unnecessary dependencies.

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,9 +1,5 @@
-using Api;
 using Api.Database;
-using Api.Database.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Platform;
 
 var builder = WebApplication.CreateBuilder(args);
 Platform.Platform.AddServices(builder);
--- Platform/Platform.cs
+++ Platform/Platform.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Platform.Configurations;
 
 namespace Platform;
