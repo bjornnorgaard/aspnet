47b3167041d206219b775870528114138719c840
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2025-08-12 21:10:04 +0200

    Remove test and redundant endpoints; migrate database during app startup
    - Eliminated the `test` endpoint and conditional migration logic.
    - Database migrations now always executed during startup, simplifying environment setup.

--- Api/Program.cs
+++ Api/Program.cs
@@ -16,38 +16,9 @@ builder.AddPlatform(anchor);
 
 var app = builder.Build();
 
-// Only run database migrations if not in test mode
-if (app.Environment.EnvironmentName != "Testing")
-{
-    using (var scope = app.Services.CreateScope())
-    {
-        var context = scope.ServiceProvider.GetRequiredService<Context>();
-        await context.Database.MigrateAsync();
-    }
-}
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<Context>();
+await context.Database.MigrateAsync();
 
 app.UsePlatform(anchor);
-
-app.MapGet("test", async ([FromServices] Context context) =>
-{
-    var created = await context.Todos.AddAsync(new Todo
-    {
-        Title = "Test Todo",
-        Description = "This is a test todo item.",
-        IsCompleted = false,
-        CreatedAt = DateTimeOffset.UtcNow,
-    });
-
-    await context.SaveChangesAsync();
-
-    return Results.Ok(new
-    {
-        created.Entity.Id,
-        created.Entity.Title,
-        created.Entity.Description,
-        created.Entity.IsCompleted,
-        created.Entity.CreatedAt
-    });
-});
-
 app.Run();
